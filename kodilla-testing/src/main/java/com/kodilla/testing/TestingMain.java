191791f313b5545b05454c3ff25872531d7093fb
Khalida <khalida@gmail.com>
2020-03-12 22:29:59 +0100

    cleaning :q

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -4,12 +4,4 @@ public class TestingMain {
     public static void main(String[] args){
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
     }
-}:Qq
-
-
-
-
-
-
-q
-:q:q
+}
